d4ed54d3ba9e8f2b3a3cb41f243f59688cf13699
bnelson <bnelson@molio.com>
2015-03-20 23:00:24 -0600

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name foodReviewerApp
+ * @description
+ * # foodReviewerApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('foodReviewerApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name foodReviewerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the foodReviewerApp
+ */
+angular.module('foodReviewerApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
